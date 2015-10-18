9a6fcb070ad3965be1d244f55c4da2fdf0b97517
Jonathan-NewPC <jonathan@fab-it.dk>
2015-10-18 22:44:10 +0200

    Intial Commit

---
+++ Json2Config.Net/ConfigManager.cs
@@ -0,0 +1,40 @@
+﻿using Json2Config.Net.Models;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Json2Config.Net
+{
+    public class ConfigManager
+    {
+        public Config Config { get; set; }
+
+        public ConfigManager(string applicationName)
+        {
+            this.Config = new Config(applicationName);
+
+            if (!Config.Exists())
+            {
+                Config.CreateDirectory();
+                Config.CreateConfig();
+            }
+            
+
+        }
+
+        public void SaveConfig<T>(T obj)
+        {
+            using (StreamWriter file = File.CreateText(Config.FullName))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Formatting = Formatting.Indented;
+
+                serializer.Serialize(file, obj);
+            }
+        }
+        public T LoadConfig<T>()
+        {
+            T value = JsonConvert.DeserializeObject<T>(Config.Json());
+            return value;
+        }
+    }
+}
---
+++ Json2Config.Net/Models/Config.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace Json2Config.Net.Models
+{
+    public class Config
+    {
+        public readonly string Name;
+        public readonly string ApplicationConfigFolder;  
+
+        public string FullName
+        {
+            get
+            {
+                return Path.Combine(ApplicationConfigFolder, Name);
+            }
+        }
+
+        public Config(string applicationName)
+        {
+            this.Name = "config.json";
+            this.ApplicationConfigFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), applicationName);
+
+        }
+
+        public bool Exists()
+        {
+            if (File.Exists(FullName))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        internal void CreateDirectory()
+        {
+            Directory.CreateDirectory(ApplicationConfigFolder);
+        }
+        internal void CreateConfig()
+        {
+            File.Create(this.FullName);
+        }
+        internal string Json()
+        {
+            using (StreamReader sr = new StreamReader(FullName))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+    }
+}
