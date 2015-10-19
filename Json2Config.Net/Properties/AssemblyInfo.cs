baa75cc11c4d09fbd7de692cc6256e81a81064af
Jonathan-NewPC <jonathan@fab-it.dk>
2015-10-19 22:46:41 +0200

    Updated AssemblyInfo for nuspec file

--- Json2Config.Net/Properties/AssemblyInfo.cs
+++ Json2Config.Net/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Json2Config.Net")]
-[assembly: AssemblyDescription("Class to Json, to Config file, easy to use.")]
+[assembly: AssemblyDescription("An easy to use, library for converting a class into Json and then save it to a local config file.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Json2Config.Net")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
