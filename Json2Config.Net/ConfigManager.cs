2f2eac90370e0c846f99c2fd12b014a76431e811
Jonathan Østrup <jesusravn@gmail.com>
2015-10-21 13:39:52 +0200

    To create on ctor, or not
    You can now specify a third parameter, that'll determine whether or not,
    the ctor will create the file, when you instantiate the configmanager

--- Json2Config.Net/ConfigManager.cs
+++ Json2Config.Net/ConfigManager.cs
@@ -9,21 +9,22 @@ namespace Json2Config.Net
         private JsonSerializer serializer;
 
         public Config ConfigFile { get; set; }
+        public bool ConfigWasCreated { get; set; }
 
         public ConfigManager(
-            string applicationName, 
-            string configFileName = "config", 
+            string applicationName,
+            string configFileName = "config",
+            bool CreateConfigIfNotExists = true,
             Formatting jsonFormat = Formatting.Indented)
         {
             this.serializer = new JsonSerializer();
-            serializer.Formatting = jsonFormat;
+            this.serializer.Formatting = jsonFormat;
 
             this.ConfigFile = new Config(applicationName, configFileName);
 
-            if (!ConfigFile.Exists())
+            if (CreateConfigIfNotExists)
             {
-                ConfigFile.CreateDirectory();
-                ConfigFile.CreateConfig();
+                this.ConfigWasCreated = CreateConfig();
             }
         }
 
@@ -37,6 +38,22 @@ namespace Json2Config.Net
         public T LoadConfig<T>()
         {
             return JsonConvert.DeserializeObject<T>(ConfigFile.RawJson());
+        }
+        public bool CreateConfig()
+        {
+            bool created = false;
+
+            if (!ConfigFile.Exists())
+            {
+                ConfigFile.CreateDirectory();
+                ConfigFile.CreateConfig();
+                created = true;
+            }
+            else
+            {
+                created = false;
+            }
+            return created;
         }
     }
 }
